0632fe0d5f1d87732926ace7cf2f4b641428e531
Md Saddam Hossain <eyarinsultana@gmail.com>
2026-05-18 20:01:31 +0600

    trusted section updated again

--- src/components/trusted/index.tsx
+++ src/components/trusted/index.tsx
@@ -3,23 +3,33 @@ import { TGalleryImage } from "@/types/trustedType";
 import TrustedCard from "../card/TrustedCard";
 import Marquee from "react-fast-marquee";
 
+const Trusted = async () => {
+  const fetchAllTrustedImage = await fetchTrustedData();
 
-const Trusted = async() => {
-    const fetchAllTrustedImage = await fetchTrustedData();
-    
-      if (!fetchAllTrustedImage) return <p>No data found</p>;
-    
-    const allTrustedImage: TGalleryImage[]= fetchAllTrustedImage
-    return (
-        <div className="mt-25 max-w-11/12 mx-auto overflow-hidden bg-gray-50 p-5">
-            <h2 className="text-center text-6xl font-bold py-5 mb-10">Trusted by organisations across the world</h2>
-         <Marquee speed={90}>
-               <div className="flex gap-6">
-                {allTrustedImage.map((trustedImg)=> <TrustedCard key={trustedImg.id} trustedImage={trustedImg}/>)}
-            </div>
-         </Marquee>
+  if (!fetchAllTrustedImage) return <p>No data found</p>;
+
+  const allTrustedImage: TGalleryImage[] = fetchAllTrustedImage;
+  return (
+    <div className="mt-25 max-w-11/12 mx-auto overflow-hidden bg-gray-50 p-5">
+      <h2 className="text-center text-6xl font-bold py-5 mb-10">
+        Trusted by organisations across the world
+      </h2>
+      <Marquee speed={90}>
+        <div className="flex gap-6">
+          {allTrustedImage.map((trustedImg) => (
+            <TrustedCard key={trustedImg.id} trustedImage={trustedImg} />
+          ))}
         </div>
-    );
+      </Marquee>
+      <Marquee speed={90} direction="right" gradient={false} className=" mt-12">
+        <div className="flex gap-6">
+          {allTrustedImage.map((trustedImg) => (
+            <TrustedCard key={trustedImg.id} trustedImage={trustedImg} />
+          ))}
+        </div>
+      </Marquee>
+    </div>
+  );
 };
 
 export default Trusted;
