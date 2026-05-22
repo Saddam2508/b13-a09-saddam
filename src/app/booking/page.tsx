e4f57de563b41a309a90aa745216a06cb73606b6
Saddam <eyarinsultana@gmail.com>
2026-05-22 22:07:48 +0600

    meta data added

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,5 +1,9 @@
 import MyBooking from "@/components/booking";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Booking page | SportNest",
+};
 const MyBookingPage = () => {
   return (
     <div className="max-w-11/12 mx-auto mt-30">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import Banner from "@/components/banner";
 import FacilityHomePage from "@/components/facilities/FacilityHomePage";
 import LeisureActivities from "@/components/leisure";
 import Trusted from "@/components/trusted";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Home page | SportNest",
+};
 export default function Home() {
   return (
     <div>
